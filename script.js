44988ae9b68d9fbb3fae6b66dfc38fc8cde7045e
mlazza <mlazzarotti@gmail.com>
2021-06-18 14:46:37 -0300

    atualizando o erro do JS, retirando o <form>

--- script.js
+++ script.js
@@ -7,21 +7,32 @@ function calcular() {
         
     let calculo = Math.round(peso / (altura * altura) * 10000);
       
-    switch(true) {
-        case calculo >= 25:
-            resultado = "Você está acima do peso. Procure uma nutricionista para regularizar o seu peso.";
-            break;
-        case calculo < 25:
-            resultado = "Você está bem, mas pode ser uma boa consultar a nutricionista para ver alguns outros exames.";
-            break;
-        case calculo < 20:
-            resultado = "Você está com baixo peso. É recomendado que faça uma reeducação alimentar";
-            break;
-        default: 
-            resultado = "Algum erro aconteceu, digite os números novamente.";
-            break;
+    if (calculo >= 25)
+        resultado = "Você está acima do peso. Procure uma nutricionista para regularizar o seu peso.";
+    else if (calculo < 25)
+        resultado = "Você está bem, mas pode ser uma boa consultar a nutricionista para ver alguns outros exames.";
+    else if (calculo < 20)
+        resultado = "Você está com baixo peso. É recomendado que faça uma reeducação alimentar";
+    else
+        resultado = "Algum erro aconteceu, digite os números novamente.";
+
+
+
+    // switch(true) {
+    //     case calculo >= 25:
+    //         resultado = "Você está acima do peso. Procure uma nutricionista para regularizar o seu peso.";
+    //         break;
+    //     case calculo < 25:
+    //         resultado = "Você está bem, mas pode ser uma boa consultar a nutricionista para ver alguns outros exames.";
+    //         break;
+    //     case calculo < 20:
+    //         resultado = "Você está com baixo peso. É recomendado que faça uma reeducação alimentar";
+    //         break;
+    //     default: 
+    //         resultado = "Algum erro aconteceu, digite os números novamente.";
+    //         break;
             
-    };
+    // };
 
     document.getElementById("result").innerHTML = resultado;
  
