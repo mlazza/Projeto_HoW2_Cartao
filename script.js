1e7b380571530955a26c68a0c8500961e2fa53be
mlazza <mlazzarotti@gmail.com>
2021-06-18 14:01:03 -0300

    atualizando script

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 function calcular() {
+    let resultado;
     let v1 = document.getElementById("peso").value;
     let v2 = document.getElementById("altura").value;
     let peso = parseInt(v1);
     let altura = parseInt(v2);
-    console.log(v1);
-    
+        
     let calculo = Math.round(peso / (altura * altura) * 10000);
-    
-    alert("Valor do calculo: " + calculo);
-
-    console.log(calculo);
-    
+      
     switch(true) {
         case calculo >= 25:
-            document.getElementById("result").innerHTML = "Você está acima do peso. Procure uma nutricionista para regularizar o seu peso.";
-            alert("passou aqui");
+            resultado = "Você está acima do peso. Procure uma nutricionista para regularizar o seu peso.";
             break;
         case calculo < 25:
-            document.getElementById("result").innerHTML = "Você está bem, mas pode ser uma boa consultar a nutricionista para ver alguns outros exames.";
+            resultado = "Você está bem, mas pode ser uma boa consultar a nutricionista para ver alguns outros exames.";
             break;
         case calculo < 20:
-            document.getElementById("result").innerHTML = "Você está com baixo peso. É recomendado que faça uma reeducação alimentar";
+            resultado = "Você está com baixo peso. É recomendado que faça uma reeducação alimentar";
             break;
         default: 
-            document.getElementById("result").innerHTML = "Algum erro aconteceu, digite os números novamente.";
+            resultado = "Algum erro aconteceu, digite os números novamente.";
             break;
             
     };
 
-    alert("fim");
-    
-    //document.getElementById("demo").innerHTML = "Hello JavaScript!"'
+    document.getElementById("result").innerHTML = resultado;
+ 
 }
