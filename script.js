d30883b07496fb97245bc970e4627f7e51ed29ed
mlazza <mlazzarotti@gmail.com>
2021-06-30 13:10:23 -0300

    atualizando calculadora, agora esta funcionando

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
+// atualizando peso e altura no texto
+
+function slider(){
+    var slider = document.getElementById("peso");
+    var output = document.getElementById("textPeso");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+
+}
+
+
+function slider2(){
+    var slider = document.getElementById("altura");
+    var output = document.getElementById("textAltura");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+
+}
+
+//funcao de calculo
 function calcular() {
     let resultado;
     let v1 = document.getElementById("peso").value;
     let v2 = document.getElementById("altura").value;
-    let peso = parseInt(v1);
-    let altura = parseInt(v2);
+    
+    let peso = parseFloat(v1);
+    let altura = parseFloat(v2);
+    alert("peso é " + peso);
+    alert("altura é " + altura);
         
-    let calculo = Math.round(peso / (altura * altura) * 10000);
-      
+    let calculo = (peso / (altura * altura));
+    
+    alert(calculo);
     if (calculo >= 25)
         resultado = "Você está acima do peso. Procure uma nutricionista para regularizar o seu peso.";
     else if (calculo < 25 && calculo >= 20)
@@ -16,42 +46,7 @@ function calcular() {
     else
         resultado = "Algum erro aconteceu, digite os números novamente.";
 
-
-
-    // switch(true) {
-    //     case calculo >= 25:
-    //         resultado = "Você está acima do peso. Procure uma nutricionista para regularizar o seu peso.";
-    //         break;
-    //     case calculo < 25:
-    //         resultado = "Você está bem, mas pode ser uma boa consultar a nutricionista para ver alguns outros exames.";
-    //         break;
-    //     case calculo < 20:
-    //         resultado = "Você está com baixo peso. É recomendado que faça uma reeducação alimentar";
-    //         break;
-    //     default: 
-    //         resultado = "Algum erro aconteceu, digite os números novamente.";
-    //         break;
-            
-    // };
-
     document.getElementById("result").innerHTML = resultado;
  
 }
 
-// atualizando peso e altura no texto
-
-function updatePesoInput(val) {
-    document.getElementById('pesoInput').value=val; 
-  }
-
-
-function slider2(){
-    var slider = document.getElementById("altura");
-    var output = document.getElementById("textAltura");
-    output.innerHTML = slider.value;
-
-    slider.oninput = function() {
-    output.innerHTML = this.value;
-    }
-
-}
